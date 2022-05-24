34fb4c8c0cd9f85ed71f916e905f3c027a074cfb
ifrahimriaz1 <ifrahimriaz.codingpixel@gmail.com>
2022-05-24 23:36:25 +0500

    Your Message

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,78 +1,21 @@
-    // Sidebar
-    $('.menu-btn').click(function(){
-      $('.side-bar').toggleClass('active');
-  });
-  
-  //slidder//
-  
-  $('#owl-carousel').owlCarousel({
-    loop: true,
-    margin: 30,
-    dots: true,
-    nav: true,
-    items: 3,
-    navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-    responsive: {
-      0: {
-        items: 1,
-        nav:true
+// slidder
+$('.neckless-div .owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  autoplay:true,
+  dots:true,
+  navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+  autoplayTimeout:1000,
+  responsive:{
+      0:{
+          items:1
       },
-      768: {
-        items: 1.4
+      600:{
+          items:2
       },
-      1170: {
-        items: 3
-        
+      1000:{
+          items:2
       }
-    }
+  }
 })
-  
-// slidder-2//
-// slidder //
-$(document).ready(function($) {
-  "use strict";
-  $('#customers-testimonials').owlCarousel( {
-      loop: true,
-      center: false,
-      items: 3,
-      margin: 30,
-      autoplay: true,
-      dots:true,
-      nav:true,
-      autoplayTimeout: 8500,
-      smartSpeed: 450,
-      navText: ['<i class="fa fa-angle-left"></i>','<i class="fa-solid fa-arrow-right"></i>'],
-      responsive: {
-        0: {
-          items: 1,
-          nav:true
-        },
-        768: {
-          items: 1.7
-        },
-        1170: {
-          items: 4
-          
-        }
-      }
-    });
-  });
-  // hamp-burger//
-  document.querySelector(".hamburger button").addEventListener("click", addActive)
-
-function addActive(){
-    let div1 = document.querySelector("nav");
-    if(div1.classList.contains("active")){
-        div1.classList.remove("active")
-    } else{
-        div1.classList.add('active');
-    }
-    let div2 = document.querySelector(".hamburger button");
-    if(div2.classList.contains("active")){
-        div2.classList.remove("active")
-    } else{
-        div2.classList.add("active");
-    }
-}
-
-// slider-coustom //
